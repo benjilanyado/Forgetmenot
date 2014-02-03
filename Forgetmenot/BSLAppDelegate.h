aae45e80f9ee63c991f2b6c6dee77839a8ce5373
Benji Lanyado <benjilanyado@gmail.com>
2014-02-03 10:42:11 +0000

    Initial Commit

---
+++ Forgetmenot/BSLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BSLAppDelegate.h
+//  Forgetmenot
+//
+//  Created by Benjamin Lanyado on 03/02/2014.
+//  Copyright (c) 2014 Benjamin Lanyado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BSLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Forgetmenot/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Forgetmenot
+//
+//  Created by Benjamin Lanyado on 03/02/2014.
+//  Copyright (c) 2014 Benjamin Lanyado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BSLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BSLAppDelegate class]));
+    }
+}
